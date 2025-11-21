e25a22efbab7b5a65c2966bc308e43f3b90b29a7
joshedmundson <josh@edmundson.org>
2025-11-21 17:16:00 +0100

    refactor: change Z_DRT to DRT_curve

--- pySIMsalabim/aux_funcs/time_domain_DRT.py
+++ pySIMsalabim/aux_funcs/time_domain_DRT.py
@@ -8,8 +8,8 @@ import numpy as np
 
 ######### Class Definitions #######################################################################
 
-class DRT_Fit:
-    """ A class that bundles together all results from fitting a Z_DRT curve to data
+class DRT_Fit_Result:
+    """ A class that bundles together all results from fitting a DRT_curve curve to data
 
     Attributes
     ----------
@@ -24,13 +24,13 @@ class DRT_Fit:
             F(x) = 0.5 * sum(rho(error_i(x)**2), i = 0, ..., n - 1)
         at the solution. By default, rho(z) = z. See scipy.optimize.least_squares for details
     m : int
-        Number of lifetimes/coefficients used in Z_DRT, given by the length of U/tau
+        Number of lifetimes/coefficients used in DRT_curve, given by the length of U/tau
     
     Methods
     -------
-    set_Z_DRT(t)
-        Calculates Z_DRT(t) using the object's attributes (self.U, self.tau, self.offset)
-        and sets self.Z_DRT equal to the result 
+    set_DRT_curve(t)
+        Calculates DRT_curve(t) using the object's attributes (self.U, self.tau, self.offset)
+        and sets self.DRT_curve equal to the result 
     """
     def __init__(self, U, tau, offset, cost, m):
         self.U = U
@@ -38,28 +38,28 @@ class DRT_Fit:
         self.offset = offset 
         self.cost = cost
         self.m = m 
-        self.Z_DRT = None
+        self.DRT_curve = None
 
-    def set_Z_DRT(self, t):
-        """ Sets self.Z_DRT = Z_DRT(t) using the objects attributes
+    def set_DRT_curve(self, t):
+        """ Sets self.DRT_curve = DRT_curve(t) using the objects attributes
 
         Parameters
         ----------
         t : float or list/numpy.ndarray, shape (n,)
-            Time values to calculate the Z_DRT curve
+            Time values to calculate the DRT_curve curve
         
         Returns
         -------
         None
         """
-        self.Z_DRT = Z_DRT(t, self.U, self.tau)
+        self.DRT_curve = DRT_curve(t, self.U, self.tau, self.offset)
 
 
 ######### Function Definitions ####################################################################
 
-def Z_DRT(t, U, tau, offset=0):
+def DRT_curve(t, U, tau, offset=0):
     """Calculate the function 
-        Z_DRT(t) = U_1*exp(-t/tau_1) + U_2*exp(-t/tau_2) + ... + U_m*exp(-t/tau_m) + offset
+        DRT_curve(t) = U_1*exp(-t/tau_1) + U_2*exp(-t/tau_2) + ... + U_m*exp(-t/tau_m) + offset
     
     Parameters
     ----------
@@ -75,8 +75,8 @@ def Z_DRT(t, U, tau, offset=0):
         
     Returns 
     -------
-    calculated_Z_DRT : numpy.ndarray with shape (n,)
-        Z_DRT at all time values in t
+    calculated_DRT_curve : numpy.ndarray with shape (n,)
+        DRT_curve at all time values in t
     """
     
     # Check to make sure all input has been formatted as numpy arrays
@@ -88,17 +88,17 @@ def Z_DRT(t, U, tau, offset=0):
     elif not isinstance(t, np.ndarray):
         t = np.array(t)
     
-    # Calculate Z_DRT(t) for all values in t
-    calculated_Z_DRT = (U @ np.exp(-np.outer(1/tau, t))) + offset
+    # Calculate DRT_curve(t) for all values in t
+    calculated_DRT_curve = (U @ np.exp(-np.outer(1/tau, t))) + offset
     
-    return calculated_Z_DRT
+    return calculated_DRT_curve
 
 
-def fit_Z_DRT(t, y, U0, tau0, offset0=0, set_Z_DRT=True, **kwargs):
-    """Fits a Z_DRT(t) curve to a function y(t)
+def fit_DRT_curve(t, y, U0, tau0, offset0=0, set_DRT_curve=True, **kwargs):
+    """Fits an DRT_curve(t) curve to a function y(t)
 
     Uses a non-linear approach to fit 
-        Z_DRT(t) = U_1*exp(-t/tau_1) + U_2*exp(-t/tau_2) + ... + U_m*exp(-t/tau_m) + offset
+        DRT_curve(t) = U_1*exp(-t/tau_1) + U_2*exp(-t/tau_2) + ... + U_m*exp(-t/tau_m) + offset
     to a function y by finding the optimal values for the parameters 
         U = [U_1, U_2, ..., U_m] 
         tau = [tau_1, tau_2, ..., tau_m]
@@ -108,21 +108,21 @@ def fit_Z_DRT(t, y, U0, tau0, offset0=0, set_Z_DRT=True, **kwargs):
     t : (list/numpy.ndarray) with shape (n,)
         The time values over which the function y is known
     y : list/numpy.ndarray with shape (n,)
-        The values of y(t) that Z_DRT will be fitted to
+        The values of y(t) that DRT_curve will be fitted to
     U0 : list/numpy.ndarray with shape (m,)
         Initial guess of U values for fitting 
     tau0 : list/numpy.ndarray with shape (m,)
         Initial guess of tau values for fitting
     offset0 : float (optional)
-        Initial guess of the Z_DRT offset value
-    set_Z_DRT : bool (optional)
-        If true, will set the Z_DRT attribute of the returned DRT_Fit object using t
+        Initial guess of the DRT_curve offset value
+    set_DRT_curve : bool (optional)
+        If true, will set the DRT_curve attribute of the returned DRT_Fit_Result object using t
     kwargs
         Keyword arguments passed to scipy.optimize.least_squares for fitting
     
     Returns
     -------
-    drt_fit : DRT_Fit 
+    drt_fit : DRT_Fit_Result 
         With attributes as follows
             U : numpy.ndarray, shape (m,)
                 Fitted [U_1, U_2, ..., U_m] array
@@ -135,7 +135,7 @@ def fit_Z_DRT(t, y, U0, tau0, offset0=0, set_Z_DRT=True, **kwargs):
                     F(x) = 0.5 * sum(rho(error_i(x)**2), i = 0, ..., n - 1)
                 at the solution. By default, rho(z) = z. See scipy.optimize.least_squares for details
             m : int
-                Number of lifetimes/coefficients used in Z_DRT, given by the length of U/tau
+                Number of lifetimes/coefficients used in DRT_curve, given by the length of U/tau
     """
     
     # Make sure passed parameters are numpy.ndarrays
@@ -149,53 +149,57 @@ def fit_Z_DRT(t, y, U0, tau0, offset0=0, set_Z_DRT=True, **kwargs):
     initial_params = np.concatenate((U0, tau0, [offset0]))
     
     # Create the error function 
-    error = lambda x : y - Z_DRT(t, x[:m], x[m:-1], offset=x[-1])
+    error = lambda x : y - DRT_curve(t, x[:m], x[m:-1], offset=x[-1])
     
     # Fit the params using scipy. Note x_scale='jac' makes for better fits than the default value
     fit = so.least_squares(error, x0=initial_params, x_scale='jac', **kwargs)
     
     # Store the fit results
-    drt_fit = DRT_Fit(fit.x[:m], fit.x[m:-1], fit.x[-1], fit.cost, m)
+    drt_fit = DRT_Fit_Result(fit.x[:m], fit.x[m:-1], fit.x[-1], fit.cost, m)
 
-    # Set the Z_DRT curves for each DRT_Fit result if calc_Z_DRT=True
-    drt_fit.set_Z_DRT(t) if set_Z_DRT else None
+    # Set the DRT_curve curves for each DRT_Fit_Result result if calc_DRT_curve=True
+    drt_fit.set_DRT_curve(t) if set_DRT_curve else None
     
     return drt_fit
 
 
-def multi_fit_Z_DRT(t, y, m_values, set_Z_DRT=True, **kwargs):
-    """ Runs fit_Z_DRT over multiple m values 
+def multi_fit_DRT_curve(t, y, m_values, U_scale_factor=0, offset0=0, set_DRT_curve=True, **kwargs):
+    """ Runs fit_DRT_curve over multiple m values 
     
     Parameters
     ----------
     t : list or numpy.ndarray, shape (n,)
         Time values over which y(t) is known
     y : list or numpy.ndarray, shape(n, )
-        y(t) values each Z_DRT curve is fitted to
+        y(t) values each DRT_curve curve is fitted to
     m_values : list, shape (i,)
-        Various m values for fit_Z_DRT, where m is the number of coefficients/lifetimes
-        in Z_DRT(t)
-    set_Z_DRT : bool (optional)
-        If true, will set the Z_DRT attribute of the returned DRT_Fit objects using t
+        Various m values for fit_DRT_curve, where m is the number of coefficients/lifetimes
+        in DRT_curve(t)
+    U_scale_factor : float
+        Controls the scale for the intial guess of the coefficients U0 passed to fit_DRT_curve
+    offset0 : float 
+        Initial guess for the offset passed to fit_DRT_curve
+    set_DRT_curve : bool (optional)
+        If true, will set the DRT_curve attribute of the returned DRT_Fit_Result objects using t
     kwargs 
         Keyword arguments passed to scipy.optimize.least_squares for fitting
 
     Returns
     -------
-    fits : list of DRT_Fit, shape (i,)
-        List of DRT_Fit objects, one for each passed m value
+    fits : list of DRT_Fit_Result, shape (i,)
+        List of DRT_Fit_Result objects, one for each passed m value
     """
     fits = []
     
     # Fit the params for each m
     for m in m_values:
         # Default estimate U, tau, and offset 
-        U0 = np.zeros(m)
+        U0 = np.ones(m)*U_scale_factor
         tau0 = np.geomspace(t[0], t[-1], m)
-        offset0 = 0
+        offset0 = offset0
         
         # Fit the curve
-        fit = fit_Z_DRT(t, y, U0, tau0, offset0=offset0, set_Z_DRT=set_Z_DRT, **kwargs)
+        fit = fit_DRT_curve(t, y, U0, tau0, offset0=offset0, set_DRT_curve=set_DRT_curve, **kwargs)
         
         # Add to fits
         fits.append(fit)
